0c0a8dc6dd5c6cf0744fdc071dc215361663ef0c
drzainulabdeen <iamzainulabdeen@gmail.com>
2022-12-30 00:15:38 +0500

    added synony generation functionality

---
+++ parameters.py
@@ -0,0 +1,18 @@
+# Path and name of the input file
+
+input_file = 'data/Synonym_App_Input_File.csv'
+
+# Path and name of output file
+
+output_file = 'data/output.csv'
+
+# These are all model parameters
+
+prompt = 'List 2 synonyms for following word:'
+engine = 'text-davinci-003'
+temp = 0.7
+top_p = 1.0
+tokens = 400
+freq_pen = 0.400		# frequency penalty
+pres_pen =  0.400		# presence penalty
+
